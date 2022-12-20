ee59e9be39937f3044c112ff6fd741e1f0bfbc49
Prathima111 <61008920+Prathima111@users.noreply.github.com>
2022-12-20 20:38:08 +0530

    Create PrepBuddy and Socks.java

---
+++ PrepBuddy and Socks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String args[]) throws IOException {
+    
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    while(n-->0)
+    {
+      int i=s.nextInt();
+      System.out.println(i+1);
+    }
+    
+  }
+}
